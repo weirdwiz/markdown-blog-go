37d1fa21ee9d5835fb372b19fb07b63e020ede1f
Divyansh Kamboj <agilephoenix815.dk@gmail.com>
2018-11-03 15:57:29 +0530

    Parse Markdown

--- server.go
+++ server.go
@@ -2,12 +2,36 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"io/ioutil"
 	"net/http"
 	"time"
+
+	markdown "gopkg.in/russross/blackfriday.v2"
 )
 
+// Post : struct for a blog post
+type Post struct {
+	html     string
+	date     time.Time
+	title    string
+	markdown string
+}
+
+func (post Post) parse() error {
+
+	return nil
+}
+
 func main() {
+	parseMarkdown()
 	mux := http.NewServeMux()
+
+	// Fileserver
+	files := http.FileServer(http.Dir(config.Static))
+	mux.Handle("/static/", http.StripPrefix("/static/", files))
+
+	// Handlers
 	mux.HandleFunc("/", index)
 
 	server := &http.Server{
@@ -21,5 +45,25 @@ func main() {
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello Dudes")
+	templates := template.Must(template.ParseFiles("templates/layout.html"))
+	templates.ExecuteTemplate(w, "layout", template.HTML(parseMarkdown()))
+}
+
+func parseMarkdown() string {
+	file, err := ioutil.ReadFile("test.md")
+	if err != nil {
+		panic(err)
+	}
+	parsed := markdown.Run(file)
+	return string(parsed)
+}
+
+func generateHTML(w http.ResponseWriter, data interface{}, filenames ...string) {
+	var files []string
+	for _, file := range filenames {
+		files = append(files, fmt.Sprintf("templates/%s.html", file))
+	}
+
+	templates := template.Must(template.ParseFiles(files...))
+	templates.ExecuteTemplate(w, "layout", data)
 }
